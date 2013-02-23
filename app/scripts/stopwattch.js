4f0268a45ab33112e71bbcec47caaec0c6708381
Bart Nijssen <bart@bartnijssen.com>
2013-02-23 11:11:29 +0100

    Added flot-support with JSON realtime
    Flotr2 is not implemented because: not compatible with JQM and damn
    hard to implement with JSOS.

--- app/scripts/stopwattch.js
+++ app/scripts/stopwattch.js
@@ -1,53 +1,13 @@
-$(document).ready(function () {
-    // we use an inline data source in the example, usually data would
-    // be fetched from a server
-    var data = [], totalPoints = 300;
+function fetchData() {
+    $.ajax({
+            type:'get', 
+            dataType: "json", 
+            url:'json.txt',
+            success: function(data) {
+                $.plot($("#graph"), [data], { xaxis: { mode: "time" } } );
+            }
+        });
+   //setTimeout(fetchData, 100); //enable for auto-updating
+};
 
-    function getData() {
-        return getRandomData();
-    }
-
-    // Return bogus values for now
-    function getRandomData() {
-        if (data.length > 0)
-            data = data.slice(1);
-
-        // do a random walk
-        while (data.length < totalPoints) {
-            var prev = data.length > 0 ? data[data.length - 1] : 50;
-            var y = prev + Math.random() * 10 - 5;
-            if (y < 0)
-                y = 0;
-            if (y > 100)
-                y = 100;
-            data.push(y);
-        }
-
-        // zip the generated y values with the x values
-        var res = [];
-        for (var i = 0; i < data.length; ++i)
-            res.push([i, data[i]])
-        return res;
-    }
-
-    // setup control widget
-    var updateInterval = 30;
-
-    // setup plot
-    var options = {
-        series: { shadowSize: 0 }, // drawing is faster without shadows
-        yaxis: { min: 0, max: 100 },
-        xaxis: { show: false }
-    };
-    var plot = $.plot($("#graph"), [ getData() ], options);
-
-    function update() {
-        plot.setData([ getRandomData() ]);
-        // since the axes don't change, we don't need to call plot.setupGrid()
-        plot.draw();
-        
-        setTimeout(update, updateInterval);
-    }
-
-    update();
-});
+fetchData();
